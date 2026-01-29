ddaad5dcad5b4afb5aec1831759d7a7d9c7d8ffb
prachiii0418-star <prachiii.0418@gmail.com>
2026-01-29 11:51:32 +0530

    Add files via upload

---
+++ JS DAY 8/style.js
@@ -0,0 +1,7 @@
+const profile = document.getElementById('profile');
+
+profile.style.backgroundColor = 'navy';
+profile.style.color = 'white';
+profile.style.padding = '15px';
+
+profile.style.textAlign = 'center';
